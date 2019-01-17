b7d263092c1b06ef94dd9210d0e18427a6a88c95
jtessadori <30410690+jtessadori@users.noreply.github.com>
2019-01-17 14:09:25 +0100

    Working version, but still no luck with tests
    The code should already be working as is, but apparently I am now unable to generate motor imagery on command. Testing with older setups, it would seem that both error potentials and visual-related activity are produced and can be detected normally, so the idea now is to change the interface and use a classifier that takes into account both motor imagery estimations and error potentials and combine them to produce an estimation of actual intention. This requires essentially scrapping the whole interface and starting anew (yeah!), so I shall go to work now.

--- TSclassifier.m
+++ TSclassifier.m
@@ -1,7 +1,44 @@
 classdef TSclassifier < handle
     %% January, 2019 Jacopo Tessadori
     methods (Static)
-        function BAcc=crossVal(data,lbls,nPartitions)
+        function s=projectData(inData,RS)
+            % Projects all entries in inData to tangent space defined by RS
+            if iscell(inData)
+                s=zeros(size(inData,1),size(inData{1},2)*(size(inData{1},2)+1)/2);
+                for currTrial=1:size(inData,1)
+                    s(currTrial,:)=RS.project(cov(inData{currTrial}));
+                end
+            else
+                s=zeros(size(inData,1),size(inData,3)*(size(inData,3)+1)/2);
+                for currTrial=1:size(inData,1)
+                    s(currTrial,:)=RS.project(cov(squeeze(inData(currTrial,:,:))));
+                end
+            end
+        end
+        
+        function featsIdx=selectFeatures(inData,lbls,weights)
+            % Compute one-way ANOVA for each feature, then sort
+            % p-values
+            p=zeros(size(inData,2),1);
+            for currFeat=1:size(inData,2)
+                p(currFeat)=anova1(inData(:,currFeat),lbls,'off');
+            end
+            [pSorted,pIdx]=sort(p);
+            
+            % Compute number of features to keep
+            th=0.05;
+            nFeatures=2;
+            while true
+                if sum(weights(pIdx(1:nFeatures)))/size(inData,2)*th<=pSorted(nFeatures)||nFeatures==length(p)
+                    break;
+                else
+                    nFeatures=nFeatures+1;
+                end
+            end
+            featsIdx=pIdx(1:nFeatures);
+        end
+        
+        function [BAcc,classEst]=crossVal(data,lbls,nPartitions)
             % Generate partitions
             C.NumTestSets=nPartitions;
             C.groups=ceil(linspace(1/length(lbls),C.NumTestSets,length(lbls)));
@@ -20,10 +57,7 @@ classdef TSclassifier < handle
                 RS=riemannSpace(trainData);
                 
                 % Project both train and test data to tangent space
-                s=zeros(length(lbls),size(data,3)*(size(data,3)+1)/2);
-                for currTrial=1:size(data,1)
-                    s(currTrial,:)=RS.project(cov(squeeze(data(currTrial,:,:))));
-                end
+                s=TSclassifier.projectData(data,RS);
                 
                 % Split projections in training and testing set
                 trainProj=s(C.training(currP),:);
@@ -33,38 +67,118 @@ classdef TSclassifier < handle
                 [coeff,trainProj,latent]=pca(trainProj);
                 testProj=testProj*coeff;
                 
-                % Compute one-way ANOVA for each feature, then sort
-                % p-values
-                p=zeros(size(trainProj,2));
-                for currFeat=1:size(trainProj,2)
-                    p(currFeat)=anova1(trainProj(:,currFeat),trainLbls,'off');
-                end
-                [pSorted,pIdx]=sort(p);
-                
-                % Compute number of features to keep
-                th=0.05;
-                nFeatures=1;
-                while true
-                    if sum(latent(pIdx(1:nFeatures)))/size(trainProj,2)*th<=pSorted(nFeatures)
-                        break;
-                    else
-                        nFeatures=nFeatures+1;
-                    end
-                end
-                featsIdx=pIdx(1:nFeatures);
+                % Perform feature selection
+                featsIdx=TSclassifier.selectFeatures(trainProj,trainLbls,latent);
+                fprintf('nFeats: %d\n',length(featsIdx));
                 
                 % Leave only selected features
                 trainProj=trainProj(:,featsIdx);
                 testProj=testProj(:,featsIdx);
                 
-                % Perform training (second order polynomial svm, in this
-                % case)
+                % Perform training 
 %                 clsfr=fitcsvm(trainProj,trainLbls,'Standardize',true,'KernelScale','auto','KernelFunction','polynomial','PolynomialOrder',2);
 %                 clsfr=fitcsvm(trainProj,trainLbls,'KernelFunction', 'gaussian','KernelScale', 16,'BoxConstraint', 1,'Standardize', true,'ClassNames', [0; 1]);
                 clsfr=fitcdiscr(trainProj,trainLbls);
+%                 clsfr = fitcensemble(trainProj,trainLbls,'Method','RobustBoost','NumLearningCycles',300,'Learners','Tree','RobustErrorGoal',0.002,'RobustMaxMargin',1);
                 
                 % Classify test projections
                 classEst(C.test(currP))=clsfr.predict(testProj);
+                
+                % Evaluate results for current partition
+                partBAcc=testAcc(testLbls,classEst(C.test(currP)));
+                fprintf('Fold %d/%d BAcc: %0.2f\n',currP,C.NumTestSets,partBAcc);
+            end
+            BAcc=testAcc(lbls,classEst);
+            fprintf('\n Cross-val BAcc: %0.2f\n\n',BAcc);
+        end
+        
+        function plotTSproj(data,lbls)
+            % Compute Riemann-mean on training data only
+            RS=riemannSpace(data);
+            
+            % Project both train and test data to tangent space
+            data=TSclassifier.projectData(data,RS);
+            
+            % Perform PCA
+            [~,data]=pca(data);
+            
+            % Compute one-way ANOVA for each feature, then sort
+            % p-values
+            p=zeros(size(data,2),1);
+            for currFeat=1:size(data,2)
+                p(currFeat)=anova1(data(:,currFeat),lbls,'off');
+            end
+            [~,pIdx]=sort(p);
+            
+            % Plot first three features
+            clrs='rgkbcmy';
+            lblValues=unique(lbls);
+            hold on
+            for currValue=1:length(lblValues)
+                plot3(data(lbls==lblValues(currValue),pIdx(1)),data(lbls==lblValues(currValue),pIdx(2)),data(lbls==lblValues(currValue),pIdx(3)),['o',clrs(currValue)]);
+            end
+        end
+        
+        function [BAcc,classEst]=crossValTime(data,lbls,nPartitions)
+            % Generate partitions
+            C.NumTestSets=nPartitions;
+            C.groups=ceil(linspace(1/length(lbls),C.NumTestSets,length(lbls)));
+            C.training=@(currGroup)C.groups~=currGroup;
+            C.test=@(currGroup)C.groups==currGroup;
+            
+            % Perform cross-validation
+            classEst=zeros(size(lbls));
+            for currP=1:C.NumTestSets
+                % Recover training and testing sets
+                trainData=data(C.training(currP),:,:);
+                trainLbls=lbls(C.training(currP));
+                testLbls=lbls(C.test(currP));
+                
+                % Compute class means
+                classTags=unique(lbls);
+                classMeans=zeros(length(classTags),size(data,2),size(data,3));
+                for currClass=1:length(classTags)
+                    classMeans(currClass,:,:)=median(trainData(trainLbls==classTags(currClass),:,:));
+                end
+                clsfr.classMeans=classMeans;
+                
+                % Construct super-trials
+                superTrainFeats=zeros(size(data,1),size(data,2),size(data,3)*(length(classTags)+1));
+                for currTrial=1:size(data,1)
+                    superTrainFeats(currTrial,:,:)=cat(2,reshape(permute(classMeans,[1,3,2]),[],size(data,2))',squeeze(data(currTrial,:,:)));
+                end
+                
+                % Compute Riemann-mean on training data only
+                RS=riemannSpace(superTrainFeats);
+                
+                % Project both train and test data to tangent space
+                s=TSclassifier.projectData(superTrainFeats,RS);
+                
+                % Split projections in training and testing set
+                trainProj=s(C.training(currP),:);
+                testProj=s(C.test(currP),:);
+                
+                % Perform PCA
+                [coeff,trainProj,latent]=pca(trainProj);
+                testProj=testProj*coeff;
+                
+                % Perform feature selection
+                featsIdx=TSclassifier.selectFeatures(trainProj,trainLbls,latent);
+                fprintf('nFeats: %d\n',length(featsIdx));
+                
+                % Leave only selected features
+                trainProj=trainProj(:,featsIdx);
+                testProj=testProj(:,featsIdx);
+                
+                % Perform training
+%                 clsfr=fitcsvm(trainProj,trainLbls,'Standardize',true,'KernelScale','auto','KernelFunction','polynomial','PolynomialOrder',2);
+%                 clsfr=fitcsvm(trainProj,trainLbls,'KernelFunction', 'gaussian','KernelScale', 16,'BoxConstraint', 1,'Standardize', true,'ClassNames', [0; 1]);
+                clsfr=fitcdiscr(trainProj,trainLbls);
+%                 clsfr = fitcensemble(trainProj,trainLbls,'Method','RobustBoost','NumLearningCycles',300,'Learners','Tree','RobustErrorGoal',0.15,'RobustMaxMargin',1);
+                
+                % Classify test projections
+                classEst(C.test(currP))=clsfr.predict(testProj);
+                clear clsfr
                 
                 % Evaluate results for current partition
                 partBAcc=testAcc(testLbls,classEst(C.test(currP)));
--- riemannSpace.m
+++ riemannSpace.m
@@ -8,15 +8,34 @@ classdef riemannSpace < handle
     end
     
     methods
-        function obj=riemannSpace(relWins)
-            % Compute covariance matrices
-            covMats=cell(size(relWins,1),1);
-            for currTrial=1:size(relWins,1)
-                covMats{currTrial}=cov(squeeze(relWins(currTrial,:,:)));
+        function obj=riemannSpace(relWins,varargin)
+            % If only one argument is passed, relWins are expected to be
+            % time windows. If a second argument reading 'cov' is passed,
+            % relWins are expected to be covariance matrices, already
+            if nargin==2&&strcmp(varargin{1},'cov')
+                if length(size(relWins))==3
+                    covMats=mat2cell(relWins,ones(size(relWins,1),1),size(relWins,2),size(relWins,3));
+                    covMats=cellfun(@(x)squeeze(x),covMats,'UniformOutput',0);
+                else
+                    covMats{1}=relWins;
+                end
+            else
+                % Compute covariance matrices
+                if iscell(relWins)
+                    covMats=cell(size(relWins));
+                    for currTrial=1:length(relWins)
+                        covMats{currTrial}=cov(relWins{currTrial});
+                    end
+                else
+                    covMats=cell(size(relWins,1),1);
+                    for currTrial=1:size(relWins,1)
+                        covMats{currTrial}=cov(squeeze(relWins(currTrial,:,:)));
+                    end
+                end
             end
             
             % Compute Riemannian mean
-            obj.Rmean=karcher(covMats{1:size(relWins,1)});
+            obj.Rmean=karcher(covMats{1:size(covMats,1)});
             
             % Compute sqrt of Rmean and inv(Rmean)
             obj.sqrtMean=obj.sqrt(obj.Rmean);
@@ -41,20 +60,20 @@ classdef riemannSpace < handle
         function expP=exp(inData)
             % Performs exp of symmetric positive-definite matrices
             [V,D]=eig(inData);
-            expP=V*diag(exp(diag(D)))*V';
+            expP=V*diag(exp(abs(diag(D))))*V';
         end
         
         function logP=log(inData)
             % Performs exp of symmetric positive-definite matrices
             [V,D]=eig(inData);
-            logP=V*diag(log(diag(D)))*V';
+            logP=V*diag(log(abs(diag(D))))*V';
         end
         
         function sqrtP=sqrt(inData)
             % Performs (extended) square root of symmetric
             % positive-definite matrices
             [V,D]=eig(inData);
-            sqrtP=V*diag(sqrt(diag(D)))*V';
+            sqrtP=V*diag(sqrt(abs(diag(D))))*V';
         end
     end
 end
